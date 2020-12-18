cf1456c6d51e1b4a5ccaffb97ea5f10d825bb08b
Linden Bryon <linden@rushcliff.com>
2020-12-18 20:36:07 +0000

    setting up a proxy api and connectting to mongoose

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
+//Set up default mongoose connection
+var mongoDB = 'mongodb+srv://linden:!shoulderpress123@cluster0.xzo1a.mongodb.net/user_authentication?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+var db = mongoose.connection;
+console.log("db", db);
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/api/home', function(req, res) {
-  res.send('Welcome!');
+  res.send({home: 1});
 });
 app.get('/api/secret', function(req, res) {
   res.send('The password is potato');
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => console.log("listening on port 8080"));
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,19 +1,87 @@
 import { Component } from 'react';
 class SignUp extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            password_confirm: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    componentDidMount(){
+        this.callBackendApi().then((res) => {
+            console.log("success", res);
+        }).catch((err) => {
+            console.log("er", err);
+        });
+    }
+    callBackendApi = async () => {
+        
+        const response = await fetch('/api/home');
+        const body = await response.json();
+        if(response.status !== 200){
+            throw Error(body.message)
+        }
+        return body;
+    }
+    handleChange(e){
+        const target = e.target;
+        const name = target.name;
+        this.setState({
+            [name]: target.value
+        });
+        console.log(this.state);
+    }
+    checkPasswordsMatch(){
+        return this.state.password === this.state.password_confirm ? true : false
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        //we should also check if username exists here
+        if(!this.checkPasswordsMatch()){
+            //show error on frontend
+        }
+        //send data to backend
+        
+    }
     render(){
         return(
-            <form id="login-form">
+            <form id="login-form" onSubmit={this.handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="username-label" className="form-label">Username</label>
-                    <input type="text" className="form-control" name="username" id="username" aria-describedby="username" />
+                    <input 
+                        type="text"
+                        className="form-control"
+                        name="username"
+                        id="username"
+                        aria-describedby="username"
+                        onChange={this.handleChange}
+                        required
+                    />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password-field" className="form-label">Password</label>
-                    <input type="password" className="form-control" name="password" id="password" />
+                    <input
+                        type="password"
+                        className="form-control"
+                        name="password"
+                        id="password"
+                        onChange={this.handleChange} 
+                        required
+                    />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password-field" className="form-label">Confirm Password</label>
-                    <input type="password" className="form-control" name="password_confirm" id="password-confirm" />
+                    <input
+                        type="password"
+                        className="form-control"
+                        name="password_confirm"
+                        id="password-confirm" 
+                        onChange={this.handleChange}
+                        required
+                    />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
